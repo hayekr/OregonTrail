3ecfbdff65f07c36671dab5ccfeb0267f179c5a9
c-bower <62390675+c-bower@users.noreply.github.com>
2020-03-25 23:24:21 -0400

    Create Action.java

---
+++ stubs/Action.java
@@ -0,0 +1,49 @@
+public class Action {
+    String move, buy, look, stop, checkMap;
+
+    /**
+     * Default Constructor for Action
+     *
+     */
+    Action(){ }
+
+    /**
+     * @param string indicating to move
+     * @return true if set
+     */
+    public boolean move(String move){
+        return true;
+    }
+
+    /**
+     * @param string indicating to buy
+     * @return true if set
+     */
+    public boolean buy(String buy){
+        return true;
+    }
+
+    /**
+     * @param string idicating to look
+     * @return true if set
+     */
+    public boolean look(String look){
+        return true;
+    }
+
+    /**
+     * @param string indicating to stop
+     * @return true if set
+     */
+    public boolean stop(String stop){
+        return true;
+    }
+
+    /**
+     * @param string indicating to check map
+     * @return true if set
+     */
+    public boolean checkMap(String checkMap){
+        return true;
+    }
+}
