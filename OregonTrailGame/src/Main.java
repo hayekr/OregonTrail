d9b9aa9f0d2cff9e1564808b89fe9f58e37ef7cf
Dannysia <dsiatkosky@gmail.com>
2020-03-30 06:04:36 -0400

    Initial work on actual project Basic design concept

---
+++ OregonTrailGame/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// Introduce user to game
+
+		// Allow user to fill wagon
+			// Need to add WagonFiller class - read from csv file
+
+		// Allow user to go on travels
+			// Force to go west only? It is just MVP, not real program. Ex map is 7 tiles long, start, mix of river/forest/plains, end?
+			// Will need to write MapGenerator class
+			// If not just MVP, could allow movement in all 4 directions
+	}
+}
---
+++ OregonTrailGame/src/TextProcessor.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+// Might want to make static? Since it will always be used, no point in constant instantiation of it
+// Replace all system.out with calls to Action class
+public class TextProcessor {
+	Scanner sc = new Scanner(System.in);
+
+	String[] userInputArray;
+
+	public void processText() {
+		String userInput = sc.nextLine();
+		// Modify user input to be easily parsed
+		userInputArray = userInput.toUpperCase().trim().split(" ");
+
+		// Initial Verb Parsing
+		switch(userInputArray[0]) {
+			case "LOOK": lookHandler(); break;
+			case "INVENTORY": inventoryHandler(); break;
+			case "GO":
+			case "MOVE": movementHandler(); break;
+		}
+	}
+
+	public void lookHandler() {
+		System.out.println("lookHandler");
+	}
+
+	public void inventoryHandler() {
+		System.out.println("inventoryHandler");
+	}
+
+	public void movementHandler() {
+		System.out.println("movementHandler");
+		switch(userInputArray[1]) {
+			case "UP":
+			case "NORTH": System.out.println("north"); break;
+			case "DOWN":
+			case "SOUTH": System.out.println("south"); break;
+			case "RIGHT":
+			case "EAST": System.out.println("east"); break;
+			case "LEFT":
+			case "WEST": System.out.println("west"); break;
+		}
+	}
+
+	public void error() {
+		System.out.println("error");
+	}
+}
