e275f0ff378accd325e5f67783b45525df2e0182
Dannysia <dsiatkosky@gmail.com>
2020-03-30 21:50:15 -0400

    Initial Work on GamePlay Class

---
+++ OregonTrailGame/src/GamePlay.java
@@ -0,0 +1,36 @@
+import java.beans.EventHandler;
+import java.security.cert.CertificateNotYetValidException;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class GamePlay {
+	private int westProgress = 0;
+
+
+	/**
+	 * Constructor for gameplay
+	 */
+	GamePlay(Wagon wagon) {
+		// import map
+
+
+
+		while (true) {
+			// Describe Environment
+			// replace map with whatever function in class
+			System.out.println(map.get(westProgress).getDescription());
+
+			// Allow user to make a choice
+
+			// if river stuff
+			// test to see if wagon can make it
+			// if fast river possibly have stuff fall off wagon
+
+			// if plains stuff
+
+			// if forest stuff
+		}
+	}
+
+
+}
--- OregonTrailGame/src/Main.java
+++ OregonTrailGame/src/Main.java
@@ -1,14 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	//Wagon wagon = new Wagon;
+
+	public static void main(String[] args) throws FileNotFoundException {
 		// Introducing user to game
+		introduction();
 
 		// Allow user to fill wagon
-			// Need to add WagonFiller class - read from csv file
+		// Need to add WagonFiller class - read from csv file
+		fillWagon();
 
 		// Allow user to go on travels
-			// Force to go west only? It is just MVP, not real program. Ex map is 7 tiles long, start, mix of river/forest/plains, end?
-			// Will need to write MapGenerator class
-			// If not just MVP, could allow movement in all 4 directions
+		// Force to go west only? It is just MVP, not real program. Ex map is 7 tiles long, start, mix of river/forest/plains, end?
+		// Will need to write MapGenerator class
+		// If not just MVP, could allow movement in all 4 directions
+		GamePlay game = new GamePlay();
 	}
+
+	public static void introduction() {
+		// info here
+	}
+
+	public static void fillWagon() {
+
+	}
+
 }
