12cffeb824f998f5b70bf55280f28e852c747088
Dannysia <dsiatkosky@gmail.com>
2020-03-31 21:26:14 -0400

    MVP of GamePlay class

--- OregonTrailGame/src/GamePlay.java
+++ OregonTrailGame/src/GamePlay.java
@@ -5,26 +5,25 @@ import java.util.ArrayList;
 public class GamePlay {
 	private int westProgress = 0;
 	TextProcessor tp = new TextProcessor();
+	Wagon wagon;
 
 	/**
 	 * Constructor for gameplay
 	 */
 	GamePlay(Map map, Wagon wagon) {
 
+		this.wagon = wagon;
 
 
 		while (true) {
 			// Print out description of current environment
 			System.out.println(map.getEnvironment(westProgress).getDescription());
-			// Allow user to make a choice
 
-			switch(tp.processText()) {
-				case "goWest": westProgress++; break;
-				case "inventory": System.out.println(wagon.getInventory());
-				default: System.out.println("You can't do that");
+			if (westProgress > 10) {
+				break;
 			}
 
-			//}
+			userInput();
 
 			// if river stuff
 			// test to see if wagon can make it
@@ -35,7 +34,16 @@ public class GamePlay {
 			// if forest stuff
 			//break;
 		}
+
+		System.out.println("Congratulations! You made it!");
 	}
 
 
+	private void userInput() {
+		switch(tp.processText()) {
+			case "goWest": westProgress++; break;
+			case "inventory": System.out.println(wagon.getInventory());
+			default: System.out.println("You can't do that");
+		}
+	}
 }
